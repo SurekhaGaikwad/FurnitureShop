33ca918bd371224ee46dd5edd8b773d7123a9498
ProjectGRP3ri <hiwalen@gmail.com>
2022-01-11 17:33:48 +0530

    init

--- FurnitureShop/Home/views.py
+++ FurnitureShop/Home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-
+from Products.models import Studyroom, Livingroom
 def Home(request):
-    return render(request,'home.html')
+    livingroom = Livingroom.objects.filter(Discount__gte = 1)
+    studyroom = Studyroom.objects.filter(Discount__gte = 1)
 
+    context = {        
+            'livingroom':livingroom,
+            'studyroom':studyroom
+            }
 
+    return render(request,'home.html', context)
+
+# def DiscountItem(request):
+    # livingroom = Livingroom.objects.get(Discount__gte = 0 )
+    # studyroom = Studyroom.objects.filter(Discount__gte = 0)
+  
 
 # Create your views here.
 
